9cd9cca7a1c7b7413b65daf1e072d6af60340ae2
Metehan BOLAT <73827610+metehanie@users.noreply.github.com>
2022-06-09 00:24:54 +0300

    Initial commit

---
+++ app/src/main/java/com/metehanbolat/recyclerviewautomaticswipeanimation/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.metehanbolat.recyclerviewautomaticswipeanimation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.metehanbolat.recyclerviewautomaticswipeanimation.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+    }
+}
